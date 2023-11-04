e7b32080c64fb5b7e4f5b429b68f8f3ea2756eca
alfonsoalarcon <alfonsoalarcon@msn.com>
2023-11-04 10:41:20 +0100

    Add new code g8 countries

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ import pandas as pd
 file_path = 'global_electricity_statistics.csv'
 df = pd.read_csv(file_path, sep=",", skipinitialspace=True)
 
-print(df.head())
+df1 = df[["Country","Features","2016","2017","2018","2019","2020","2021"]]
+values=["Canada","France", "Germany", "Italy", "Japan", "Russia", "United States", "United Kingdom"]
+df2 = df1.loc[(df1["Country"].isin(values)) & (df1["Features"] == "net generation")]
+
+print(df2)
+
