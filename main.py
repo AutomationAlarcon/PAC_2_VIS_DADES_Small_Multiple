06ad9e0bbc1edf82a03514aacf122a0abfbbcdf7
alfonsoalarcon <alfonsoalarcon@msn.com>
2023-11-11 11:54:53 +0100

    Add new code g8 countries

--- main.py
+++ main.py
@@ -6,28 +6,44 @@ file_path = 'global_electricity_statistics.csv'
 df = pd.read_csv(file_path, sep=",", skipinitialspace=True)
 
 df1 = df[["Country","Features","2016","2017","2018","2019","2020","2021"]]
+x1 = ["United States", "Canada","France", "Germany", "Italy", "Japan", "United Kingdom"]
+
+df_net_consumption = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "net consumption")]
+df_imports = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "imports")]
+df_exports = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "exports")]
+df_net_imports = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "net imports")]
+df_net_exports = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "net exports")]
+df_net_generation = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "net generation")]
+x = list(df_net_generation["Country"])
+print(df_net_generation)
+print(x)
+
+df_net_generation['Country'] = df_net_generation['Country'].astype('category')
+df_net_generation['Features'] = df_net_generation['Features'].astype('category')
+df_net_generation['2016'] = df_net_generation['2016'].astype(float)
+df_net_generation['2017'] = df_net_generation['2017'].astype(float)
+df_net_generation['2018'] = df_net_generation['2018'].astype(float)
+df_net_generation['2019'] = df_net_generation['2019'].astype(float)
+df_net_generation['2020'] = df_net_generation['2020'].astype(float)
+df_net_generation['2021'] = df_net_generation['2021'].astype(float)
+
+print(df_net_generation.dtypes)
+
+# df_net_consumption['2021'] = df_net_consumption['2021'].astype(float)
+# df_imports['2021'] = df_imports['2021'].astype(float)
+# df_exports['2021'] = df_exports['2021'].astype(float)
+# df_net_imports['2021'] = df_net_imports['2021'].astype(float)
+# df_net_exports['2021'] = df_net_exports['2021'].astype(float)
+
+# y = [list(df_net_generation["2021"]), list(df_net_consumption["2021"]), list(df_imports["2021"]),
+#      list(df_exports["2021"]), list(df_net_imports["2021"]), list(df_net_exports["2021"])]
+# legend_value = ["Net generation", "Net consumption", "Imports", "Exports", "Net imports", "Net exports"]
+
+y = [list(df_net_generation["2016"]), list(df_net_generation["2017"]), list(df_net_generation["2018"]),
+     list(df_net_generation["2019"]), list(df_net_generation["2020"]), list(df_net_generation["2021"])]
 legend_value = list(df1.columns.values)
 legend_value = legend_value[2:]
 print(legend_value)
-
-x1 = ["United States", "Russia", "Canada","France", "Germany", "Italy", "Japan", "United Kingdom"]
-df2 = df1.loc[(df1["Country"].isin(x1)) & (df1["Features"] == "net generation")]
-x = list(df2["Country"])
-print(df2)
-print(x)
-
-df2['Country'] = df2['Country'].astype('category')
-df2['Features'] = df2['Features'].astype('category')
-df2['2016'] = df2['2016'].astype(float)
-df2['2017'] = df2['2017'].astype(float)
-df2['2018'] = df2['2018'].astype(float)
-df2['2019'] = df2['2019'].astype(float)
-df2['2020'] = df2['2020'].astype(float)
-df2['2021'] = df2['2021'].astype(float)
-
-print(df2.dtypes)
-
-y = [list(df2["2016"]), list(df2["2017"]), list(df2["2018"]), list(df2["2019"]), list(df2["2020"]), list(df2["2021"])]
 
 plt.figure(figsize=(15, 12))
 for i in range(len(y)):
@@ -36,9 +52,9 @@ for i in range(len(y)):
     plt.subplot(3, 2, i+1)
     #plt.plot(x, y[i], label=legend_value[i], marker='X')
     bars = plt.barh(x, y[i], color='gray')
-    plt.title("Net generation in the year " + legend_value[i])
-    plt.ylabel("G8 Countries")
-    plt.xlabel("Net generation (billion kWh)")
+    plt.title("Net electricity generation in the year " + legend_value[i])
+    plt.ylabel("G7 Countries")
+    plt.xlabel("Net electricity generation (billion kWh)")
     #plt.legend(loc="lower right")
     plt.xticks(rotation='horizontal')
     plt.xticks(np.arange(0, 5500, 500))
@@ -49,6 +65,6 @@ for i in range(len(y)):
         plt.text(xval + 0.05, bar.get_y() + bar.get_height() / 2, round(xval, 2), ha='left', va='center', fontsize=10)
 
 plt.tight_layout()
-plt.savefig("g8_net_generation", dpi=100, bbox_inches='tight')
+plt.savefig("g7_net_generation", dpi=100, bbox_inches='tight')
 
 
